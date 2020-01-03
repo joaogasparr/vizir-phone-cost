711f3eba8270b3d2c293456757ea49049aa58ed8
João Vitor Gaspar <joao.vgaspar@hotmail.com>
2020-01-03 15:12:16 -0300

    [chore] name of validation fields

--- backend/src/app/validators/Rate/Update.js
+++ backend/src/app/validators/Rate/Update.js
@@ -3,10 +3,10 @@ import * as Yup from 'yup';
 export default async (req, res, next) => {
   try {
     const schema = Yup.object().shape({
-      origin: Yup.number('Origin must be a number')
+      origin_id: Yup.number('Origin must be a number')
         .integer('Origin must be a integer')
         .positive('Origin must be a positive number'),
-      destiny: Yup.number('Destiny must be a number')
+      destiny_id: Yup.number('Destiny must be a number')
         .integer('Destiny must be a integer')
         .positive('Destiny must be a positive number'),
       price: Yup.number('Price must be a number').positive(
